e2d3f44d6384c21b4fa96d63c4f685b38092a5eb
Tonya <tonya_verkooijen@mail.ru>
2025-03-27 20:24:44 +0300

    start

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <Text  style={{display: "flex", height: "100%", alignItems: "center", justifyContent: "center", backgroundColor: "lavender"}}>T-PLANNER</Text>
+  );
+}
